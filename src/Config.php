9093e645943c3221c9e7a8fae869d1a04573b9dd
Zdeněk Drahoš <drahoszdenek@gmail.com>
2016-01-12 09:08:51 +0100

    Config - load default config

--- src/Config.php
+++ src/Config.php
@@ -2,13 +2,29 @@
 
 namespace Edge\QA;
 
+use Symfony\Component\Yaml\Yaml;
+
 class Config
 {
+    private $defaultConfig;
+
+    public function __construct()
+    {
+        $config = file_get_contents(__DIR__ . '/../.phpqa.yml');
+        $this->defaultConfig = Yaml::parse($config);
+    }
+
     public function value($path)
     {
+        $result = $this->defaultConfig;
+        foreach (explode('.', $path) as $key) {
+            $result = $result[$key];
+        }
+        return $result;
     }
 
     public function path($path)
     {
+        return $this->value($path);
     }
 }
---
+++ tests/ConfigTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Edge\QA;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+    public function testShouldLoadDefaultConfig()
+    {
+        $config = new Config();
+        assertThat($config->value('phpcpd.minLines'), is(greaterThan(0)));
+        assertThat($config->value('phpcpd.minTokens'), is(greaterThan(0)));
+        assertThat($config->value('phpcs.standard'), is(nonEmptyString()));
+        assertThat($config->path('phpmd.standard'), is(nonEmptyString()));
+    }
+}
