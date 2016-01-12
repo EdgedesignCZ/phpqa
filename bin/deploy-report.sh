4badb78f6333edb32c97a5d76a90a782cef9902e
Zdeněk Drahoš <drahoszdenek@gmail.com>
2016-01-12 12:45:28 +0100

    Script for deploying report to gh-pages

---
+++ bin/deploy-report.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Deploy latest report to gh-pages (http://edgedesigncz.github.io/phpqa/report/phpqa.html)
+#
+# Usage:
+#  $ bin/deploy-report.sh ../github-pages/phpqa
+
+repository=$1
+
+deploy() {
+    checkout_repository
+    build_report
+    copy_artifacts
+    publish_changes
+}
+
+checkout_repository() {
+    (
+        cd $repository
+        git checkout gh-pages
+    )
+}
+
+build_report() {
+    phpqa --report
+}
+
+copy_artifacts() {
+    cp -R build/*.html $repository/report
+    cp -R build/*.svg $repository/report
+}
+
+publish_changes() {
+    (
+        cd $repository
+        git add .
+        git commit -m "Report - `date +'%Y-%m-%d %H:%M:%S'`"
+        git push origin gh-pages
+    )
+}
+
+deploy
