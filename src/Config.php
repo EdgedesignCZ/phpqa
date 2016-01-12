cf22e9917a82f4910cc046bc5821cb2612177b20
Zdeněk Drahoš <drahoszdenek@gmail.com>
2016-01-12 09:39:15 +0100

    Config - refactoring

--- src/Config.php
+++ src/Config.php
@@ -6,49 +6,57 @@ use Symfony\Component\Yaml\Yaml;
 
 class Config
 {
-    private $configDir;
-    private $defaultConfig;
-
-    private $overridenDir;
-    private $overridenConfig = array();
+    private $configs = array();
 
     public function __construct()
     {
-        $this->configDir = __DIR__ . '/../';
-        $config = file_get_contents("{$this->configDir}.phpqa.yml");
-        $this->defaultConfig = Yaml::parse($config);
+        $this->loadCustomConfig(__DIR__ . '/../');
     }
 
-    public function loadCustomConfig($configDir)
+    public function loadCustomConfig($directory)
     {
-        $this->overridenDir = $configDir . '/';
-        if (file_exists("{$this->overridenDir}.phpqa.yml")) {
-            $config = file_get_contents("{$this->overridenDir}.phpqa.yml");
-            $this->overridenConfig = Yaml::parse($config);
+        $configDir = $directory . '/';
+        $configFile = "{$configDir}.phpqa.yml";
+        if (file_exists($configFile)) {
+            $config = Yaml::parse(file_get_contents($configFile));
+            $this->configs = array_merge(
+                array($configDir => $config),
+                $this->configs
+            );
         }
     }
 
     public function value($path)
     {
-        list($dir, $value) = $this->get($path);
-        return $value;
+        return $this->get(
+            $path,
+            function ($value) {
+                return $value;
+            }
+        );
     }
 
     public function path($path)
     {
-        list($dir, $file) = $this->get($path);
-        return realpath("{$dir}{$file}");
+        return $this->get(
+            $path,
+            function ($file, $dir) {
+                return realpath("{$dir}{$file}");
+            }
+        );
     }
 
-    private function get($path)
+    private function get($path, $extractor)
     {
-        $overriden = $this->getValue($this->overridenConfig, $path);
-        return $overriden ?
-            array($this->overridenDir, $overriden) :
-            array($this->configDir, $this->getValue($this->defaultConfig, $path));
+        foreach ($this->configs as $dir => $config) {
+            $value = $this->findInConfig($config, $path);
+            if ($value) {
+                return $extractor($value, $dir);
+            }
+        }
     }
 
-    private function getValue(array $config, $path)
+    private function findInConfig(array $config, $path)
     {
         $result = $config;
         foreach (explode('.', $path) as $key) {
