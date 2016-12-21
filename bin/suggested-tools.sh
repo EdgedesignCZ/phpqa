4dd444433486901daf112ea8af56b3e6eda55588
Zdeněk Drahoš <drahoszdenek@gmail.com>
2016-12-21 15:04:55 +0100

    Parallel lint - tool is not pre installed

---
+++ bin/suggested-tools.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Usage:
+#  $ bin/suggested-tools.sh install
+#  $ bin/suggested-tools.sh remove
+
+mode="$1"
+
+if [ $mode = "install" ]
+then
+    echo "Installing suggested tools"
+    composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter
+else
+    echo "Removing suggested tools"
+    composer remove jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter
+fi
