fbafeca5d77e8bdfb81eb7bbb9af650d20e3178e
Zdeněk Drahoš <drahoszdenek@gmail.com>
2017-04-23 13:47:00 +0200

    Robo - dont use autoescaping in robo v1
    Breaking The arg(), args() and option() methods in CommandArguments now escape the values passed in to them. There is now a rawArg() method if you need to add just one argument that has already been escaped.

---
+++ src/Task/RoboAdapter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Edge\QA\Task;
+
+class RoboAdapter
+{
+    private $isVersionOne;
+
+    public function __construct()
+    {
+        $this->isVersionOne = method_exists('Robo\Common\CommandArguments', 'rawArg');
+    }
+
+    // Robo v1 escapes the values
+    public function arg($exec, $arg)
+    {
+        if ($this->isVersionOne) {
+            return $exec->rawArg($arg);
+        } else {
+            return $exec->arg($arg);
+        }
+    }
+}
