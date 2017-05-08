d8d764a6373d01165f4883c74a2c96049b8f43db
Zdeněk Drahoš <drahoszdenek@gmail.com>
2017-05-08 10:52:30 +0200

    php-cs-fixer - add suggested tool

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./phpqa --verbose --report --config tests/.travis --tools phpcs:0,phpmd:0,phpcpd:0,parallel-lint:0,phpstan,phpmetrics,phploc,pdepend
+./phpqa --verbose --report --config tests/.travis --tools phpcs:0,php-cs-fixer:0,phpmd:0,phpcpd:0,parallel-lint:0,phpstan,phpmetrics,phploc,pdepend
--- bin/suggested-tools.sh
+++ bin/suggested-tools.sh
@@ -9,8 +9,8 @@ mode="$1"
 if [ $mode = "install" ]
 then
     echo "Installing suggested tools"
-    composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan
+    composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer
 else
     echo "Removing suggested tools"
-    composer remove jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan
+    composer remove jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer
 fi
