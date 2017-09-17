408df5919ea9479976723dd8d3958820a7f5ce56
MacFJA <macfja@gmail.com>
2017-09-18 00:22:37 +0200

    Add internalClass to prevent error + add Psalm in CI

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./phpqa --verbose --report --config tests/.travis --tools phpcs:0,php-cs-fixer:0,phpmd:0,phpcpd:0,parallel-lint:0,phpstan,phpmetrics:0,phploc,pdepend,phpunit:0
+./phpqa --verbose --report --config tests/.travis --tools phpcs:0,php-cs-fixer:0,phpmd:0,phpcpd:0,parallel-lint:0,phpstan,phpmetrics:0,phploc,pdepend,phpunit:0,psalm
