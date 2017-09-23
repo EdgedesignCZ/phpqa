4a80fe6e41e56eb3b5b0925d6c646d3e82a73439
Zdeněk Drahoš <drahoszdenek@gmail.com>
2017-09-23 18:10:33 +0200

    Docker - spike building docker image

--- bin/suggested-tools.sh
+++ bin/suggested-tools.sh
@@ -2,14 +2,16 @@
 
 # Usage:
 #  $ bin/suggested-tools.sh install
+#  $ bin/suggested-tools.sh install --update-no-dev
 #  $ bin/suggested-tools.sh remove
 
 mode="$1"
+requireMode="$2"
 
 if [ $mode = "install" ]
 then
     echo "Installing suggested tools"
-    composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer:~2.2 vimeo/psalm
+    composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer:~2.2 vimeo/psalm $requireMode
 else
     echo "Removing suggested tools"
     composer remove jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer vimeo/psalm
