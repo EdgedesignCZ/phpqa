94e9b49744f5a44d90e4682fb3a77495a90780e4
Zdeněk Drahoš <drahoszdenek@gmail.com>
2018-02-04 11:02:58 +0100

    CI - install suggested tools in PHP 7.0 like in docker build
    Docker build error
    https://travis-ci.org/EdgedesignCZ/phpqa/jobs/337159089#L1161
    
    Similar composer issue
    https://github.com/EdgedesignCZ/phpqa/pull/99/commits/d0c1220adf0fcee2c061548f3e27882e5bd419b5

--- bin/suggested-tools.sh
+++ bin/suggested-tools.sh
@@ -11,7 +11,13 @@ requireMode="$2"
 if [ $mode = "install" ]
 then
     echo "Installing suggested tools"
-    composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer:~2.2 vimeo/psalm sensiolabs/security-checker $requireMode
+    if [ ! -z "$requireMode" ]; then
+        # docker build OR travis + php 7.0 OR symfony2 (default composer.lock)
+        composer require symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2 jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan:~0.8.0 friendsofphp/php-cs-fixer:~2.2 vimeo/psalm sensiolabs/security-checker $requireMode
+    else
+        # symfony 3
+        composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer:~2.2 vimeo/psalm sensiolabs/security-checker
+    fi
 else
     echo "Removing suggested tools"
     composer remove jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer vimeo/psalm sensiolabs/security-checker
