70757c649666febe2cda8a526ccdee6768945242
Zdeněk Drahoš <drahoszdenek@gmail.com>
2018-05-19 09:48:32 +0200

    CI - install psalm v1
    v2 causes failed builds
    https://travis-ci.org/EdgedesignCZ/phpqa/jobs/381003238#L603
    https://travis-ci.org/EdgedesignCZ/phpqa/jobs/381003239#L739
    
    v2 released 7 days ago
    https://github.com/vimeo/psalm/releases/tag/2.0.0

--- bin/suggested-tools.sh
+++ bin/suggested-tools.sh
@@ -13,10 +13,10 @@ then
     echo "Installing suggested tools"
     if [ ! -z "$requireMode" ]; then
         # docker build OR travis + php 7.0 OR symfony2 (default composer.lock)
-        composer require symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2 jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan:~0.8.0 friendsofphp/php-cs-fixer:~2.2 vimeo/psalm sensiolabs/security-checker $requireMode
+        composer require symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2 jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan:~0.8.0 friendsofphp/php-cs-fixer:~2.2 vimeo/psalm:~1 sensiolabs/security-checker $requireMode
     else
         # symfony 3
-        composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer:~2.2 vimeo/psalm sensiolabs/security-checker
+        composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer:~2.2 vimeo/psalm:~1 sensiolabs/security-checker
     fi
 else
     echo "Removing suggested tools"
