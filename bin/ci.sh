38e16205479924d18ef183fa7636d929f013bb4f
Zdeněk Drahoš <drahoszdenek@gmail.com>
2018-07-05 13:12:08 +0200

    CI - don't analyze phpstan errors
    Latest phpstan version introduces new errors, e.g.
    > Cannot call method xpath() on SimpleXMLElement|false.	
    > https://travis-ci.org/EdgedesignCZ/phpqa/jobs/400361356#L1763
    
    I want stable build, so ignore all errors. Just like for psalm
    https://github.com/EdgedesignCZ/phpqa/commit/eb93a3d

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./phpqa --verbose --report --config tests/.travis --tools phpmetrics:0,phploc,phpcs:1,php-cs-fixer:0,phpmd:0,pdepend,phpcpd:0,phpstan:0,phpunit:0,psalm,security-checker:0,parallel-lint:0
+./phpqa --verbose --report --config tests/.travis --tools phpmetrics:0,phploc,phpcs:1,php-cs-fixer:0,phpmd:0,pdepend,phpcpd:0,phpstan,phpunit:0,psalm,security-checker:0,parallel-lint:0
