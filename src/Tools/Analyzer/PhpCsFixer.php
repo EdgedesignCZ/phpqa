94a9f376f163669d0c12f6efb23f1ca17861515a
Marek Grudzinski <marek.grudzinski@dlr.de>
2019-02-22 13:27:43 +0100

    Fix executing php-cs-fixer w/o path if config file given

--- src/Tools/Analyzer/PhpCsFixer.php
+++ src/Tools/Analyzer/PhpCsFixer.php
@@ -18,9 +18,7 @@ class PhpCsFixer extends \Edge\QA\Tools\Tool
     public function __invoke()
     {
         $configFile = $this->config->value('php-cs-fixer.config');
-        if ($configFile) {
-            $analyzedDir = $this->options->getAnalyzedDirs(' ');
-        } else {
+        if (!$configFile) {
             $analyzedDirs = $this->options->getAnalyzedDirs();
             $analyzedDir = reset($analyzedDirs);
             if (count($analyzedDirs) > 1) {
@@ -33,17 +31,20 @@ class PhpCsFixer extends \Edge\QA\Tools\Tool
         }
         $args = [
             'fix',
-            $analyzedDir,
             'verbose' => '',
             'format' => $this->options->isSavedToFiles ? 'junit' : 'txt',
         ];
         if ($configFile) {
             $args['config'] = $configFile;
         } else {
-            $args += [
-                'rules' => $this->config->value('php-cs-fixer.rules'),
-                'allow-risky' => $this->config->value('php-cs-fixer.allowRiskyRules') ? 'yes' : 'no',
-            ];
+            $args = array_merge(
+                $args,
+                [
+                    $analyzedDir,
+                    'rules' => $this->config->value('php-cs-fixer.rules'),
+                    'allow-risky' => $this->config->value('php-cs-fixer.allowRiskyRules') ? 'yes' : 'no',
+                ]
+            );
         }
         if ($this->config->value('php-cs-fixer.isDryRun')) {
             $args['dry-run'] = '';
