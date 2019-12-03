534147ff09e717bbd582ef013ffc21a2241c1cb2
Beerend Lauwers <blauwers@netvlies.net>
2019-12-03 12:46:59 +0100

    Pass the "--names" option to PHPCPD.

--- src/Tools/Analyzer/Phpcpd.php
+++ src/Tools/Analyzer/Phpcpd.php
@@ -13,12 +13,20 @@ class Phpcpd extends \Edge\QA\Tools\Tool
 
     public function __invoke()
     {
+        $phpcpdNames = array_map(
+            function ($extension) {
+                return "*.{$extension}";
+            },
+            explode(',', $this->config->csv('phpqa.extensions'))
+        );
+
         $args = array(
             'progress' => '',
             $this->options->ignore->bergmann(),
             $this->options->getAnalyzedDirs(' '),
             'min-lines' => $this->config->value('phpcpd.minLines'),
             'min-tokens' => $this->config->value('phpcpd.minTokens'),
+            'names' => $phpcpdNames,
         );
         if ($this->options->isSavedToFiles) {
             $args['log-pmd'] = $this->tool->getEscapedXmlFile();
