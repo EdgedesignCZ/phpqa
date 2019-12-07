7d6b83c6cff62cc39f92a77e8c52a2a4b4069a73
Zdeněk Drahoš <drahoszdenek@gmail.com>
2019-12-07 13:11:32 +0100

    phpstan - fix supporting v0.12 (can't use phpstan-src)
    https://travis-ci.org/EdgedesignCZ/phpqa/jobs/621970707#L791
      [InvalidArgumentException]                                                   
      Could not find a matching version of package phpstan/phpstan-src. Check the package spelling, your version constraint and that the package is available in a stability which matches your minimum-stability (stable).

--- bin/suggested-tools.sh
+++ bin/suggested-tools.sh
@@ -16,7 +16,7 @@ then
         composer require symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2 jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan:~0.8.0 friendsofphp/php-cs-fixer:~2.2 vimeo/psalm:~1 sensiolabs/security-checker:~5 $requireMode
     else
         # symfony 3
-        composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan-src friendsofphp/php-cs-fixer:~2.2 vimeo/psalm sensiolabs/security-checker
+        composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan nette/neon friendsofphp/php-cs-fixer:~2.2 vimeo/psalm sensiolabs/security-checker
     fi
 else
     echo "Removing suggested tools"
