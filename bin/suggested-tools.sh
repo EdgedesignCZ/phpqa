26dc2682f7371f55089be8b3b17f083a9369252e
Zdeněk Drahoš <drahoszdenek@gmail.com>
2019-12-08 15:07:08 +0100

    CI - try phpstan 0.12 on php 7.2
    https://travis-ci.org/EdgedesignCZ/phpqa/jobs/622274667#L1833
      simplexml_load_file(): build//phpstan.xml:1: parser error : Start tag expec

--- bin/suggested-tools.sh
+++ bin/suggested-tools.sh
@@ -13,7 +13,7 @@ then
     echo "Installing suggested tools"
     if [ ! -z "$requireMode" ]; then
         # docker build OR travis + php 7.0 OR symfony2 (default composer.lock)
-        composer require symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2 jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan:~0.8.0 friendsofphp/php-cs-fixer:~2.2 vimeo/psalm:~1 sensiolabs/security-checker:~5 $requireMode
+        composer require symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2 jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan:~0.12.0 nette/neon friendsofphp/php-cs-fixer:~2.2 vimeo/psalm:~1 sensiolabs/security-checker:~5 $requireMode 
     else
         # symfony 3
         composer require jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter phpstan/phpstan nette/neon friendsofphp/php-cs-fixer:~2.2 vimeo/psalm sensiolabs/security-checker
