37fbdf7a7f77ec3f382208f056cff17271e58f36
Zdeněk Drahoš <drahoszdenek@gmail.com>
2021-02-02 13:03:55 +0100

    Tools - fix supporting phpcpd v6
    https://github.com/EdgedesignCZ/phpqa/pull/218/checks?check_run_id=1814348576#step:5:210
    Installation request for phpunit/phpunit ~7.0...
    
    https://github.com/EdgedesignCZ/phpqa/pull/218/checks?check_run_id=1814385995
    PHP Fatal error: Declaration of Edge\QA\OptionsTest::setUp() must be compatible with PHPUnit\Framework\TestCase::setUp(): void in OptionsTest.php on line 25
    
    https://github.com/EdgedesignCZ/phpqa/pull/218/checks?check_run_id=1814481036#step:6:48
    Unknown option "--progress"
    
    https://github.com/sebastianbergmann/phpcpd/blob/master/ChangeLog.md
    6.0.0 - 2020-08-13
    Removed:
    The --names CLI option has been removed; use the --suffix CLI option instead
    The --progress CLI option has been removed

--- src/Tools/Analyzer/Phpcpd.php
+++ src/Tools/Analyzer/Phpcpd.php
@@ -2,6 +2,8 @@
 
 namespace Edge\QA\Tools\Analyzer;
 
+use Edge\QA\Tools\GetVersions;
+
 class Phpcpd extends \Edge\QA\Tools\Tool
 {
     public static $SETTINGS = array(
@@ -14,24 +16,35 @@ class Phpcpd extends \Edge\QA\Tools\Tool
     public function __invoke()
     {
         $args = array(
-            'progress' => '',
             $this->options->ignore->bergmann(),
             $this->options->getAnalyzedDirs(' '),
             'min-lines' => $this->config->value('phpcpd.minLines'),
             'min-tokens' => $this->config->value('phpcpd.minTokens'),
         );
+        $isOlderVersion = $this->isOlderVersionThanV6();
         $phpcpdNames = array_map(
-            function ($extension) {
-                return "*.{$extension}";
+            function ($extension) use ($isOlderVersion) {
+                return $isOlderVersion ? "*.{$extension}" : ".{$extension}";
             },
             array_filter(explode(',', $this->config->csv('phpqa.extensions')))
         );
+        if ($isOlderVersion) {
+            $args['progress'] = '';
+        }
         if ($phpcpdNames) {
-            $args['names'] = \Edge\QA\escapePath(implode(',', $phpcpdNames));
+            $namesOptions = $isOlderVersion ? 'names' : 'suffix';
+            $args[$namesOptions] = \Edge\QA\escapePath(implode(',', $phpcpdNames));
         }
         if ($this->options->isSavedToFiles) {
             $args['log-pmd'] = $this->tool->getEscapedXmlFile();
         }
         return $args;
+    }
+
+    private function isOlderVersionThanV6()
+    {
+        $versions = new GetVersions();
+        $composerVersion = $versions->getToolVersion(self::$SETTINGS);
+        return $composerVersion && version_compare($composerVersion, '6', '<');
     }
 }
