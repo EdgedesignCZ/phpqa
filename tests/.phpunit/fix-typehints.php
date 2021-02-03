0464fdfa54446d2ecfe0076d4ce8f07f079e0365
Zdeněk Drahoš <drahoszdenek@gmail.com>
2021-02-03 09:19:01 +0100

    CI - test latest tools on php 7.4
    https://github.com/EdgedesignCZ/phpqa/pull/219/checks?check_run_id=1821013012#step:6:7
    PHP Fatal error:  Declaration of Edge\QA\OptionsTest::setUp() must be compatible with PHPUnit\Framework\TestCase::setUp(): void
    
    https://github.com/EdgedesignCZ/phpqa/pull/219/checks?check_run_id=1821044568#step:6:829
    Ignore not installed phpcpd, it will be fixed in another PR.

---
+++ tests/.phpunit/fix-typehints.php
@@ -0,0 +1,21 @@
+<?php
+
+$files = [
+    'OptionsTest.php',
+    'Report/ReportTest.php',
+];
+foreach ($files as $file) {
+    $path = __DIR__ . "/../{$file}";
+    $compatiblePhp = str_replace(
+        [
+            'function setUp()',
+            'function tearDown()',
+        ],
+        [
+            'function setUp(): void',
+            'function tearDown(): void',
+        ],
+        file_get_contents($path)
+    );
+    file_put_contents($path, $compatiblePhp);
+}
