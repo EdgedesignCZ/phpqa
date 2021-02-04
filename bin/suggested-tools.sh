0390dc554d53958e2ceb8daa218c6193ff1c6b8b
Zdeněk Drahoš <drahoszdenek@gmail.com>
2021-02-04 10:51:48 +0100

    Tools - install suggested tools on php5
    - don't restrict phpunit version, composer should select correct version
    - install suggested tools for php5
    
    https://github.com/EdgedesignCZ/phpqa/pull/220/checks?check_run_id=1829726513#step:5:74
      Package phpstan/phpstan at version  has a PHP requirement incompatible with your PHP version (5.6.40)

--- bin/suggested-tools.sh
+++ bin/suggested-tools.sh
@@ -8,15 +8,25 @@
 mode="$1"
 requireMode="$2"
 
+PHP7=${PHP7:-"yes"}
+PSALM_VERSION=${PSALM_VERSION:-""}
+
 if [ $mode = "install" ]
 then
     echo "Installing suggested tools"
+    TOOLS="php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter sensiolabs/security-checker vimeo/psalm$PSALM_VERSION"
+    if [[ ${PHP7} == "yes" ]]; then
+        TOOLS="${TOOLS} phpstan/phpstan nette/neon"
+    fi
+    echo $TOOLS
+    echo
+
     if [ ! -z "$requireMode" ]; then
         # docker build OR travis + php 7.0 OR symfony2 (default composer.lock)
-        composer require symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2 php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter phpstan/phpstan nette/neon friendsofphp/php-cs-fixer:~2.2 vimeo/psalm:~2 sensiolabs/security-checker $requireMode 
+        composer require symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2 $TOOLS friendsofphp/php-cs-fixer:~2.2 $requireMode 
     else
         # symfony 3
-        composer require php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter phpstan/phpstan nette/neon friendsofphp/php-cs-fixer vimeo/psalm sensiolabs/security-checker
+        composer require $TOOLS friendsofphp/php-cs-fixer
     fi
 else
     echo "Removing suggested tools"
