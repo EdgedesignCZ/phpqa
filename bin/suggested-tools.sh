13a8025900f0eac6a98d7350ff0dd2ab86fd9bc3
Zdeněk Drahoš <drahoszdenek@gmail.com>
2021-02-04 09:23:15 +0100

    Tools - cleanup restricting versions of installed suggested tools
    php-cs-fixer hotfix (https://github.com/EdgedesignCZ/phpqa/commit/6ef3519) is outdated
    New php versions are supported. It's necessary only for old installations with Symfony2:
    - friendsofphp/php-cs-fixer v2.18.2 requires symfony/filesystem ^3.0 || ^4.0 || ^5.0
    - vimeo/psalm 4.4.0 requires symfony/console ^3.4.17 || ^4.1.6 || ^5.0
    - vimeo/psalm 3.0.0 requires symfony/console ^3.0||^4.0

--- bin/suggested-tools.sh
+++ bin/suggested-tools.sh
@@ -13,10 +13,10 @@ then
     echo "Installing suggested tools"
     if [ ! -z "$requireMode" ]; then
         # docker build OR travis + php 7.0 OR symfony2 (default composer.lock)
-        composer require symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2 php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter phpstan/phpstan:~0.12.0 nette/neon friendsofphp/php-cs-fixer:~2.2 vimeo/psalm:~1 sensiolabs/security-checker:~5 $requireMode 
+        composer require symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2 php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter phpstan/phpstan nette/neon friendsofphp/php-cs-fixer:~2.2 vimeo/psalm:~2 sensiolabs/security-checker $requireMode 
     else
         # symfony 3
-        composer require php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter phpstan/phpstan nette/neon friendsofphp/php-cs-fixer:~2.2 vimeo/psalm sensiolabs/security-checker
+        composer require php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter phpstan/phpstan nette/neon friendsofphp/php-cs-fixer vimeo/psalm sensiolabs/security-checker
     fi
 else
     echo "Removing suggested tools"
