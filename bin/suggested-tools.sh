4e65cde22605e3120783fb4d542506e5f2c8f040
Zdeněk Drahoš <drahoszdenek@gmail.com>
2021-02-05 05:19:57 +0100

    Tools - installing symfony2 components with suggested tools is not necessary
    It was hotfix for old docker build https://github.com/EdgedesignCZ/phpqa/commit/94e9b49 (https://travis-ci.org/github/EdgedesignCZ/phpqa/jobs/337159089#L1161)
    It's not necessary after https://github.com/EdgedesignCZ/phpqa/commit/1afe25b and https://github.com/EdgedesignCZ/phpqa/commit/13a8025.

--- bin/suggested-tools.sh
+++ bin/suggested-tools.sh
@@ -9,14 +9,13 @@ MODE="$1"
 COMPOSER_REQUIRE_MODES="$2"
 
 PHP_VERSION=${PHP_VERSION:-"7"}
-USE_SYMFONY2_COMPONENTS=${USE_SYMFONY2_COMPONENTS:-"no"}
 UPDATE_COMPOSER=${UPDATE_COMPOSER:-"no"}
 IS_NOT_PHP5="yes"
 if [[ ${PHP_VERSION:0:1} == "5" ]]; then
     IS_NOT_PHP5="no"
 fi
 
-echo "$ PHP_VERSION=$PHP_VERSION USE_SYMFONY2_COMPONENTS=$USE_SYMFONY2_COMPONENTS UPDATE_COMPOSER=$UPDATE_COMPOSER bin/suggested-tools.sh $MODE"
+echo "$ PHP_VERSION=$PHP_VERSION UPDATE_COMPOSER=$UPDATE_COMPOSER bin/suggested-tools.sh $MODE"
 echo
 
 if [ $MODE = "install" ]
@@ -30,11 +29,6 @@ then
     TOOLS="php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter sensiolabs/security-checker friendsofphp/php-cs-fixer:>=2"
     if [[ $IS_NOT_PHP5 == "yes" ]]; then
         TOOLS="${TOOLS} vimeo/psalm:>=2 phpstan/phpstan nette/neon"
-    fi
-    if [[ $USE_SYMFONY2_COMPONENTS == "yes" ]]; then
-        # https://github.com/EdgedesignCZ/phpqa/commit/94e9b49
-        # https://github.com/EdgedesignCZ/phpqa/commit/13a8025
-        TOOLS="${TOOLS} symfony/filesystem:~2 symfony/process:~2 symfony/finder:~2"
     fi
     echo "$ composer require $TOOLS $COMPOSER_REQUIRE_MODES"
     echo
