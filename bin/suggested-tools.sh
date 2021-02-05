ed0b85bbeb7050395a851ce989523c2f1c5f7398
Zdeněk Drahoš <drahoszdenek@gmail.com>
2021-02-05 13:43:28 +0100

    Tools - refactoring suggested tools script

--- bin/suggested-tools.sh
+++ bin/suggested-tools.sh
@@ -1,39 +1,75 @@
 #!/bin/bash
 
-# Usage:
+# PHP_VERSION=number UPDATE_COMPOSER=yes|no bin/suggested-tools.sh <install|remove> <composer_require_modes>
+#
+# Help:
+#  $ bin/suggested-tools.sh
 #  $ bin/suggested-tools.sh install
 #  $ bin/suggested-tools.sh install --update-no-dev
 #  $ bin/suggested-tools.sh remove
+#
+# Update existings tools before installing suggested tools:
+#  $ UPDATE_COMPOSER="yes" bin/suggested-tools.sh
+#
+# Usage for PHP5:
+#  $ PHP_VERSION="5" bin/suggested-tools.sh
 
-MODE="$1"
+COMMAND="$0"
+SELECTED_MODE="$1"
 COMPOSER_REQUIRE_MODES="$2"
 
 PHP_VERSION=${PHP_VERSION:-"7"}
 UPDATE_COMPOSER=${UPDATE_COMPOSER:-"no"}
-IS_NOT_PHP5="yes"
-if [[ ${PHP_VERSION:0:1} == "5" ]]; then
-    IS_NOT_PHP5="no"
-fi
 
-echo "$ PHP_VERSION=$PHP_VERSION UPDATE_COMPOSER=$UPDATE_COMPOSER bin/suggested-tools.sh $MODE"
-echo
+run () {
+    TOOLS=$(get_tools)
+    if [ -z "$SELECTED_MODE" ]; then
+        show_help "install"
+        show_help "remove"
+        echo "Available tools: $TOOLS"
+        exit 1
+    fi
+    show_help $SELECTED_MODE
+    if [[ $SELECTED_MODE = "install" ]]; then
+        install "$TOOLS"
+    else
+        remove "$TOOLS"
+    fi
+}
 
-if [ $MODE = "install" ]
-then
+show_help() {
+    MODE=$1
+    echo "$ PHP_VERSION=$PHP_VERSION UPDATE_COMPOSER=$UPDATE_COMPOSER $COMMAND $MODE $COMPOSER_REQUIRE_MODES"
+    echo
+}
+
+get_tools () {
+    TOOLS="php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter sensiolabs/security-checker friendsofphp/php-cs-fixer:>=2"
+    if [[ ${PHP_VERSION:0:1} != "5" ]]; then
+        TOOLS="${TOOLS} vimeo/psalm:>=2 phpstan/phpstan nette/neon"
+    fi
+    echo $TOOLS
+}
+
+install () {
+    TOOLS=$1
     if [[ $UPDATE_COMPOSER == "yes" ]]; then
         echo "> Updating installed tools"
         echo
         composer update
     fi
     echo "> Installing suggested tools"
-    TOOLS="php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter sensiolabs/security-checker friendsofphp/php-cs-fixer:>=2"
-    if [[ $IS_NOT_PHP5 == "yes" ]]; then
-        TOOLS="${TOOLS} vimeo/psalm:>=2 phpstan/phpstan nette/neon"
-    fi
     echo "$ composer require $TOOLS $COMPOSER_REQUIRE_MODES"
     echo
     composer require $TOOLS $COMPOSER_REQUIRE_MODES
-else
+}
+
+remove () {
+    TOOLS=${1/:>=2/""}
     echo "> Removing suggested tools"
-    composer remove php-parallel-lint/php-parallel-lint php-parallel-lint/php-console-highlighter phpstan/phpstan friendsofphp/php-cs-fixer vimeo/psalm sensiolabs/security-checker
-fi
+    echo "$ composer require $TOOLS $COMPOSER_REQUIRE_MODES"
+    echo
+    composer remove $TOOLS
+}
+
+run
