94a00d90a8705a8ea114180525d209d402d6a757
Zdeněk Drahoš <drahoszdenek@gmail.com>
2021-05-14 09:12:28 +0200

    psalm - hotfix analyzing old phpunit testcase

---
+++ tests/.phpunit/fix-psalm-testcase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace
+{
+    use PHPUnit\Framework\TestCase;
+
+    abstract class PHPUnit_Framework_TestCase extends TestCase
+    {
+    }
+}
--- tests/.phpunit/fix-testcase.php
+++ tests/.phpunit/fix-testcase.php
@@ -2,5 +2,9 @@
 
 $legacyTestClass = 'PHPUnit_Framework_TestCase';
 if (!class_exists($legacyTestClass)) {
-    class_alias('PHPUnit\Framework\TestCase', $legacyTestClass);
+    // `class_alias('PHPUnit\Framework\TestCase', $legacyTestClass);` does not work for psalm:
+    // > Could not get class storage for phpunit_framework_testcase"
+    // > https://github.com/EdgedesignCZ/phpqa/runs/2581795905?check_suite_focus=true#step:7:246
+    // > https://github.com/psalm/psalm-plugin-phpunit/issues/30#issuecomment-485485187
+    include_once(__DIR__ . "/fix-psalm-testcase.php");
 }
