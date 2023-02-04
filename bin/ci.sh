5d52aad5f813c45e8e84546b883805050f4c5979
Zdeněk Drahoš <drahoszdenek@gmail.com>
2023-02-04 15:50:58 +0100

    CI - hotfix allowed security-checker errors for php5.6
    https://github.com/EdgedesignCZ/phpqa/security/dependabot/1
    twig v1.44.7 requires php >=7.2.5
    https://github.com/twigphp/Twig/blob/1.x/composer.json#L27
    
    https://github.com/EdgedesignCZ/phpqa/actions/runs/4092112363/jobs/7056622100#step:7:20
    bin/ci.sh: 11: [[: not found

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-./phpqa --config tests/.ci
+ALLOWED_SECURITY_ERRORS=${ALLOWED_SECURITY_ERRORS:-""}
+
+run () {
+    hotfix_security_checker_for_php5
+    run_phpqa
+}
+
+hotfix_security_checker_for_php5 () {
+    if [ -n "$ALLOWED_SECURITY_ERRORS" ]; then
+        echo "Updating security-checker errors to $ALLOWED_SECURITY_ERRORS..."
+        sed -i -e "s/security-checker:0/security-checker:$ALLOWED_SECURITY_ERRORS/g" tests/.ci/.phpqa.yml
+    fi
+}
+
+run_phpqa () {
+    ./phpqa --config tests/.ci
+}
+
+run
+
